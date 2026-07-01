b7aec9b23c78975f8555c32fce3a2e45c1dfd7ab
Muhamad Ganabitz Dzikri <ganadzikri@gmail.com>
2026-07-01 19:45:55 +0700

    fix cache database

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { supabaseAdmin } from "@/lib/supabase";
 import HomeClient from "@/components/HomeClient";
 
-export const revalidate = 60;
+export const dynamic = 'force-dynamic';
 
 export default async function Page() {
   const [
